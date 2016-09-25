4421ba817bba54fe759c64365711703393bfe2ef
htz guoguo <htz guoguo>
2016-09-25 16:09:55 +0800

    Initial commit

---
+++ App_Code/VB/Rest/EnvironmentalInfoService/EnvironmentalInfoService.vb
@@ -0,0 +1,25 @@
+﻿Imports System.ServiceModel.Activation
+Imports Microsoft.VisualBasic
+Imports TJX.AJHB.AppData.Contracts
+Imports TJX.AJHB.MDA.Service
+Imports System.ServiceModel
+Imports TTSK.Web.AuthorityEntry
+
+Namespace VB.Rest.EnvironmentalInfoService
+
+    <AspNetCompatibilityRequirements(requirementsmode:=Activation.AspNetCompatibilityRequirementsMode.Allowed)>
+     Public Class EnvironmentalInfoService
+        Implements IEnvironmentalInfoService
+        
+        Public Function GetEnvironmentalInfo(name As String) As TJX.AJHB.AppData.Contracts.Environmental.EnvironmentalContract Implements IEnvironmentalInfoService.GetEnvironmentalInfo
+            Dim result As New TJX.AJHB.AppData.Contracts.Environmental.EnvironmentalContract
+            If Not String.IsNullOrEmpty(name) Then
+                Dim enterprise = EnterpriseInfo.EnterpriseService.GetEnvironmentalByName(name)
+                Return New TJX.AJHB.AppData.Contracts.Environmental.EnvironmentalContract(enterprise)
+            End If
+            Return result
+        End Function
+
+    End Class
+
+End Namespace
---
+++ App_Code/VB/Rest/EnvironmentalInfoService/IEnvironmentalInfoService.vb
@@ -0,0 +1,13 @@
+﻿Imports Microsoft.VisualBasic
+Imports TJX.AJHB.AppData.Contracts
+Namespace VB.Rest.EnvironmentalInfoService
+
+    <ServiceContract()>
+      Public Interface IEnvironmentalInfoService
+
+        <OperationContract()>
+       <WebGet(responseformat:=WebMessageFormat.Json, UriTemplate:="environmental/{name}")>
+        Function GetEnvironmentalInfo(ByVal name As String) As TJX.AJHB.AppData.Contracts.Environmental.EnvironmentalContract
+
+    End Interface
+End Namespace
